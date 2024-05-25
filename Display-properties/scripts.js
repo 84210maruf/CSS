f5b6b4daeb0f7c4574b61f002d8086a234d22877
maruf <maruf84210cse@gmail.com>
2024-05-25 14:07:06 +0600

    grid and flex

--- Display-properties/scripts.js
+++ Display-properties/scripts.js
@@ -1,6 +1,8 @@
 const articles = document.querySelectorAll(".container");
 const select = document.querySelector("select");
 
+document.getElementById("demo").innerHTML = "Hello JavaScript!";
+
 function updateDisplay() {
   articles.forEach((article) => {
     article.style.display = select.value;
